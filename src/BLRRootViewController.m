e1b452380fee59aa15131b37c7713fa4eea03f6e
Eric Lee <nokeeo@me.com>
2020-06-06 18:42:53 -0700

    HIdes navigation bar.

--- src/BLRRootViewController.m
+++ src/BLRRootViewController.m
@@ -25,7 +25,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+  self.navigationController.navigationBarHidden = YES;
 }
 
 #pragma mark - BLREditoViewControllerDelegate
